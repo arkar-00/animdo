2f7979cfe40a20ea1efe1a687bf7b7aec6728780
@arkaroo <arkar.oo@mo.com.mm>
2025-06-02 10:08:51 +0630

    feat: working with animation controller

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,12 +9,25 @@ class HomePage extends StatefulWidget {
   }
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   double _buttonRadius = 100;
   final Tween<double> _pageBackgroundScale = Tween<double>(
     begin: 0.0,
     end: 1.0,
   );
+
+  AnimationController? _starIconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _starIconAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+    _starIconAnimationController!.repeat();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +35,14 @@ class _HomePageState extends State<HomePage> {
       body: Container(
         child: Stack(
           clipBehavior: Clip.none,
-          children: [_pageBackground(), _circularAnimationButton()],
+          children: [
+            _pageBackground(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              children: [_circularAnimationButton(), _starIcon()],
+            ),
+          ],
         ),
       ),
     );
@@ -29,7 +51,7 @@ class _HomePageState extends State<HomePage> {
   Widget _pageBackground() {
     return TweenAnimationBuilder(
       tween: _pageBackgroundScale,
-      curve: Curves.easeInOutCubicEmphasized,
+      curve: Curves.easeInOutExpo,
       duration: Duration(seconds: 1),
 
       builder: (_context, double _scale, _child) {
@@ -61,6 +83,19 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+    );
+  }
+
+  Widget _starIcon() {
+    return AnimatedBuilder(
+      animation: _starIconAnimationController!.view,
+      builder: (_buildContext, _child) {
+        return Transform.rotate(
+          angle: _starIconAnimationController!.value * 2 * pi,
+          child: _child,
+        );
+      },
+      child: const Icon(Icons.star, size: 100, color: Colors.white),
     );
   }
 }
